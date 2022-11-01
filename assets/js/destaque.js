7b18debf8c259a65533502192e7964aa2a73ef39
Anderson Martins <atmmoreira.rj@gmail.com>
2022-11-01 11:49:19 -0300

    fix: search in other pages

--- assets/js/destaque.js
+++ assets/js/destaque.js
@@ -3,6 +3,7 @@ const WEBAPI_URL = `https://api.themoviedb.org/3`;
 
 const init = () => {
   loadPopularMovies();
+  search();
 }
 init();
 
@@ -20,4 +21,35 @@ function loadPopularMovies() {
         `;
       });
     });
+}
+
+function search() {
+  const search = document.querySelector('.search');
+  const searchBtn = document.querySelector('.btnSearch');
+
+  searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('.movieSearchResults').innerHTML = '';
+
+    const valueInputSearch = search.value;
+    document.querySelector('.main').classList.add('d-none');
+    document.querySelector('.searchResults').classList.remove('d-none');
+    document.querySelector('.searchResults').classList.add('d-block');
+
+    fetch(
+      `${WEBAPI_URL}/search/movie?api_key=${WEBAPI_KEY}&query=${valueInputSearch}`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        json.results.forEach((movie) => {
+          document.querySelector('.movieSearchResults').innerHTML += `
+          <div class="col-md-3 col-4">
+            <div class="card mb-3">
+              <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="..." class="img-fluid" />
+            </div>
+          </div>
+        `;
+        });
+      });
+  });
 }
